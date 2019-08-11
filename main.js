af452b607666a198b34a9d7cce5d25430fa49a9b
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2019-08-10 23:11:23 -0500

    clase y reto de objetos en Js

--- main.js
+++ main.js
@@ -10,11 +10,24 @@ var ana = {
   edad: 18
 }
 
-function imprimirNombreEnMayusculas({
-  nombre
-}) {
+function imprimirNombreEnMayusculas(persona) {
+  // var nombre = persona.nombre
+  var {
+    nombre
+  } = persona
+
   console.log(nombre.toUpperCase())
 }
 
 imprimirNombreEnMayusculas(jose)
 imprimirNombreEnMayusculas(ana)
+
+function imprimirNombreYEdad(persona) {
+  var {
+    nombre,
+    edad
+  } = persona
+  console.log(`Hola mi nombre es ${nombre} y mi edad es ${edad}`)
+}
+
+imprimirNombreYEdad(jose)
