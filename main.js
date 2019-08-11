a909ba6cf61c5bb9f9f91e54473243f9877e7826
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2019-08-10 22:26:53 -0500

    clase de variables de Numeros

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-var nombre = 'Jose',
-  apellido = 'Acevedo'
+var edad = 27
 
-var nombreEnMayusculas = nombre.toUpperCase()
-var apellidoEnMinusculas = apellido.toLowerCase()
+// edad = edad + 1
+edad += 1
 
-var primeraLetraDelNombre = nombre.charAt(0)
-var cantidaDeLetrasDelNombre = nombre.length
+var peso = 75
 
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+// peso = peso - 2
+peso -= 2
 
-var str = nombre.substr(1, 2)
+// Operaciones con variables
+var sandwich = 1
+
+peso = peso + sandwich
+
+var jugarFutbol = 3
+
+peso -= jugarFutbol
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino * 100 * 3) / 100
+var totalStr = total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadDePorcionesPorPersona = pizza / persona
