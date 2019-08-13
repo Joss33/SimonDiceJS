24a41f2f95d74657c460f95173f0e7c28f05e2ed
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2019-08-13 08:40:14 -0500

    creando clases en Js

--- main.js
+++ main.js
@@ -1,68 +1,23 @@
-var sasha = {
-  nombre: "Sasha",
-  apellido: "Lifszyc",
-  altura: 1.72,
-  cantidadDeLibros: 82
+function Persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura = altura;
+}
+
+Persona.prototype.saludar = function() {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
 };
 
-var alan = {
-  nombre: "Alan",
-  apellido: "Perez",
-  altura: 1.86,
-  cantidadDeLibros: 72
+Persona.prototype.soyAlto = function() {
+  return this.altura > 1.8;
 };
 
-var martin = {
-  nombre: "Martin",
-  apellido: "Gomez",
-  altura: 1.85,
-  cantidadDeLibros: 67
-};
+var jose = new Persona("jose", "Acevedo", 1.7);
+var erika = new Persona("Erika", "Luna", 1.9);
+var ana = new Persona("Ana", "Escorcia", 1.95);
 
-var dario = {
-  nombre: "Dario",
-  apellido: "Ruiz",
-  altura: 1.71,
-  cantidadDeLibros: 85
-};
+jose.saludar();
 
-var vicky = {
-  nombre: "Vicky",
-  apellido: "Zapata",
-  altura: 1.56,
-  cantidadDeLibros: 50
-};
-
-var paula = {
-  nombre: "Paula",
-  apellido: "Barros",
-  altura: 1.76,
-  cantidadDeLibros: 182
-};
-
-const esAlta = persona => persona.altura > 1.8;
-
-var personas = [sasha, alan, martin, dario, vicky, paula];
-
-var personasAltas = personas.filter(esAlta);
-
-const pasarAlturaCms = persona => {
-  persona.altura *= 100;
-  return persona;
-};
-
-var personasCms = personas.map(pasarAlturaCms);
-
-// var acum = 0;
-
-// for (var i = 0; i < personas.length; i++) {
-//   acum += personas[i].cantidadDeLibros;
-// }
-
-const reducer = (acum, personas) => {
-  return acum + personas.cantidadDeLibros;
-};
-
-var totalDeLibros = personas.reduce(reducer, 0);
-
-console.log(`En total todos tienen ${totalDeLibros} libros`);
+jose.soyAlto();
+erika.soyAlto();
+ana.soyAlto();
