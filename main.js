c01df4ae378d3c1347fda1796a08df0d70540006
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2019-08-13 08:15:37 -0500

    clase de reducir un array a un valor en JS

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
 var sasha = {
   nombre: "Sasha",
   apellido: "Lifszyc",
-  altura: 1.72
+  altura: 1.72,
+  cantidadDeLibros: 82
 };
 
 var alan = {
   nombre: "Alan",
   apellido: "Perez",
-  altura: 1.86
+  altura: 1.86,
+  cantidadDeLibros: 72
 };
 
 var martin = {
   nombre: "Martin",
   apellido: "Gomez",
-  altura: 1.85
+  altura: 1.85,
+  cantidadDeLibros: 67
 };
 
 var dario = {
   nombre: "Dario",
   apellido: "Ruiz",
-  altura: 1.71
+  altura: 1.71,
+  cantidadDeLibros: 85
 };
 
 var vicky = {
   nombre: "Vicky",
   apellido: "Zapata",
-  altura: 1.56
+  altura: 1.56,
+  cantidadDeLibros: 50
 };
 
 var paula = {
   nombre: "Paula",
   apellido: "Barros",
-  altura: 1.76
+  altura: 1.76,
+  cantidadDeLibros: 182
 };
 
 const esAlta = persona => persona.altura > 1.8;
@@ -47,4 +53,16 @@ const pasarAlturaCms = persona => {
 
 var personasCms = personas.map(pasarAlturaCms);
 
-console.log(personasCms);
+// var acum = 0;
+
+// for (var i = 0; i < personas.length; i++) {
+//   acum += personas[i].cantidadDeLibros;
+// }
+
+const reducer = (acum, personas) => {
+  return acum + personas.cantidadDeLibros;
+};
+
+var totalDeLibros = personas.reduce(reducer, 0);
+
+console.log(`En total todos tienen ${totalDeLibros} libros`);
