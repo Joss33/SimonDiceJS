8e1e1d61d6968092c775e5c493fb4d2d5a7cefac
Jose Acevedo <josearmandoacevedoangarita@gmail.com>
2019-08-14 21:24:07 -0500

    clase de como pasar funciones por parametros

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ class Persona {
     this.apellido = apellido;
     this.altura = altura;
   }
-  saludar() {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola, me llamo ${nombre} ${apellido}`);
+    if (fn) {
+      fn(nombre, apellido);
+    }
   }
   soyAlto() {
     return this.altura > 1.8;
@@ -19,13 +23,26 @@ class Desarrollador extends Persona {
     // this.apellido = apellido;
     // this.altura = altura;
   }
-  saludar() {
-    console.log(
-      `Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrolador/a`
-    );
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola, me llamo ${nombre} ${apellido} y soy desarrolador/a`);
+    if (fn) {
+      fn(nombre, apellido, true);
+    }
   }
 }
 
-// var jose = new Persona("jose", "Acevedo", 1.7);
-// var erika = new Persona("Erika", "Luna", 1.9);
-// var ana = new Desarrollador("Ana", "Escorcia", 1.95);
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen dia ${nombre} ${apellido}`);
+  if (esDev) {
+    console.log(`Ah mira, no sabia que eras Desarrollador/a`);
+  }
+}
+
+var jose = new Desarrollador("jose", "Acevedo", 1.7);
+var erika = new Persona("Erika", "Luna", 1.9);
+var ana = new Desarrollador("Ana", "Escorcia", 1.95);
+
+jose.saludar(responderSaludo);
+erika.saludar(responderSaludo);
+ana.saludar(responderSaludo);
